f11faea3db17797295a5c6104c646ceeadd42fd8
Xun <xun.ma@anu.edu.au>
2018-09-09 17:16:58 +1000

    minor fix, altered readme

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# pmgo_iv_chart_discord_bot
+# How To Install
+* Download and install [node.js](https://nodejs.org/en/)
+* Download zip file, unzip to a folder
+* Navigate to the folder in command line (try ` shift + right click` in explorer), run:
+
+`npm install discord.io winston –save`
+
+`npm install https://github.com/woor/discord.io/tarball/gateway_v6`
+
+* Create an application [here](https://discordapp.com/developers/applications/)
+* In the application created, go to Bot > Add Bot
+* Copy the token, put it in auth.json, replacing "YOUR TOKEN HERE"
+
+# How To Add To Server
+* In the application management page found client id
+* Replace the CLIENTID in this url and open in browser
+
+`https://discordapp.com/oauth2/authorize?&client_id=CLIENTID&scope=bot&permissions=8`
+
+*I'll look into the permissions and give him a lower one later 
+
+# How To Run
+* Navigate to the folder in command line (try ` shift + right click` in explorer), run:
+
+`node bot.js`
+
+# Also Check
+[How to make a Discord bot | Digital Trends](https://www.digitaltrends.com/gaming/how-to-make-a-discord-bot/)
--- bot.js
+++ bot.js
@@ -23,33 +23,33 @@ bot.on('ready', function (evt) {
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
+    if (message.substring(0, 1) === '!') {
+        var args = message.toLowerCase().substring(1).split(' ');
         var name = args[0].toLowerCase();
         var stats = basestats;
-        if(typeof args[1]!="undefined" && args[1].toLowerCase()==="alolan"){
-        	stats = alolan;
-		}
-        if(typeof(stats[name])!="undefined"){
-			var ivs = [];
-			for(i=1;i<=35;i++){
-				var atk = stats[name]["atk"];
-				var def = stats[name]["def"];
-				var hp = stats[name]["hp"];
-				ivs.push(parseInt((atk+15)*Math.sqrt(def+15)*Math.sqrt(hp+15)*Math.pow(cpm[i],2)*0.1))
-				if(i==30){
+        if (typeof args[1] != "undefined" && args[1].toLowerCase() === "alolan") {
+            stats = alolan;
+        }
+        if (typeof(stats[name]) != "undefined") {
+            var ivs = [];
+            for (i = 1; i <= 35; i++) {
+                var atk = stats[name]["atk"];
+                var def = stats[name]["def"];
+                var hp = stats[name]["hp"];
+                ivs.push(parseInt((atk + 15) * Math.sqrt(def + 15) * Math.sqrt(hp + 15) * Math.pow(cpm[i], 2) * 0.1));
+                if (i === 30) {
                     ivs.push("weather boosted:");
-				}
-			}
-			bot.sendMessage({
-                    to: channelID,
-                    message: ivs.join()
-                });
-		}else{
-			bot.sendMessage({
-                    to: channelID,
-                    message: message.substring(1) + " not found"
-                });
-		}
-	}
+                }
+            }
+            bot.sendMessage({
+                to: channelID,
+                message: ivs.join()
+            });
+        } else {
+            bot.sendMessage({
+                to: channelID,
+                message: message.substring(1) + " not found"
+            });
+        }
+    }
 });
