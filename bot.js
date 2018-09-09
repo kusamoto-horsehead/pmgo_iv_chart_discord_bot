ac964b11a7779b908307beff232f28e6ca57f649
Xun <xun.ma@anu.edu.au>
2018-09-09 13:59:11 +1000

    Added alolan forms

--- bot.js
+++ bot.js
@@ -8,6 +8,7 @@ logger.add(new logger.transports.Console, {
 });
 var basestats = require("./data/basestat.json");
 var cpm = require("./data/cp_multipier.json");
+var alolan = require("./data/alolan.json");
 logger.level = 'debug';
 // Initialize Discord Bot
 var bot = new Discord.Client({
@@ -23,21 +24,22 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
     if (message.substring(0, 1) == '!') {
-		name=message.substring(1);
-        if(typeof(basestats[name])!="undefined"){
+        var args = message.substring(1).split(' ');
+        var name = args[0].toLowerCase();
+        var stats = basestats;
+        if(typeof args[1]!="undefined" && args[1].toLowerCase()==="alolan"){
+        	stats = alolan;
+		}
+        if(typeof(stats[name])!="undefined"){
 			var ivs = [];
-			for(i=1;i<=30;i++){
-				var atk = basestats[name]["atk"];
-				var def = basestats[name]["def"];
-				var hp = basestats[name]["hp"]
+			for(i=1;i<=35;i++){
+				var atk = stats[name]["atk"];
+				var def = stats[name]["def"];
+				var hp = stats[name]["hp"];
 				ivs.push(parseInt((atk+15)*Math.sqrt(def+15)*Math.sqrt(hp+15)*Math.pow(cpm[i],2)*0.1))
-			}
-			ivs.push("weather boosted:");
-			for(i=31;i<=35;i++){
-				var atk = basestats[name]["atk"];
-				var def = basestats[name]["def"];
-				var hp = basestats[name]["hp"]
-				ivs.push(parseInt((atk+15)*Math.sqrt(def+15)*Math.sqrt(hp+15)*Math.pow(cpm[i],2)*0.1))
+				if(i==30){
+                    ivs.push("weather boosted:");
+				}
 			}
 			bot.sendMessage({
                     to: channelID,
@@ -46,7 +48,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 		}else{
 			bot.sendMessage({
                     to: channelID,
-                    message: name + " not found"
+                    message: message.substring(1) + " not found"
                 });
 		}
 	}
